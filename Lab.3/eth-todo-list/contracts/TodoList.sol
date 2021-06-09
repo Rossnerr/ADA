a6f64b1221282d0d6ba824b280b6be4347c59c35
Vasile Metei <metei1997vasile@gmail.com>
2021-06-09 20:06:06 +0300

    Added Lab.3

---
+++ Lab.3/eth-todo-list/contracts/TodoList.sol
@@ -0,0 +1,64 @@
+pragma solidity ^0.5.0;
+
+contract TodoList {
+  uint public taskCount = 0;
+
+  struct Task {
+    uint id;
+    string content;
+    bool completed;
+    Status status;
+  }
+   enum Status {
+      todo,
+      doing,
+      testing,
+      done
+   }
+
+
+  mapping(uint => Task) public tasks;
+
+  event TaskCreated(
+    uint id,
+    string content,
+    bool completed
+  );
+
+  event TaskCompleted(
+    uint id,
+    bool completed
+  );
+
+  event TaskStatusChanged(
+    uint id,
+    Status status
+  );
+
+  constructor() public {
+    createTask("demo task");
+    createTask("another demo task");
+    createTask("yeat another demo");
+  }
+
+  function createTask(string memory _content) public {
+    taskCount ++;
+    tasks[taskCount] = Task(taskCount, _content, false, Status.todo);
+    emit TaskCreated(taskCount, _content, false);
+  }
+
+  function toggleCompleted(uint _id) public {
+    Task memory _task = tasks[_id];
+    _task.completed = !_task.completed;
+    tasks[_id] = _task;
+    emit TaskCompleted(_id, _task.completed);
+  }
+
+  function changeStatus(uint _id, Status _status) public {
+    Task memory _task = tasks[_id];
+    _task.status = _status;
+    tasks[_id] = _task;
+    emit TaskStatusChanged(_id, _task.status);
+  }
+
+}
